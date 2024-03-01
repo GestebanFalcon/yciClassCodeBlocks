41799ee11cbdd2314322dbd91a350fb9d192f83f
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-02-29 23:08:44 -0500

    classBar editor component can now list and allow editing of multiple attributes

--- src/components/maker/classBar/classBar.tsx
+++ src/components/maker/classBar/classBar.tsx
@@ -5,14 +5,13 @@ interface attribute {
 
 export default function classBar({gameClass, setGameClass}: {gameClass: any, setGameClass: any}) {
     
-    const updateAttributes = (e: React.ChangeEvent<HTMLInputElement>, attribute: attribute) => {
+    const updateAttributes = (e: React.ChangeEvent<HTMLInputElement>, i: number) => {
         e.preventDefault()
         const name = e.target.name;
         const value = e.target.value;
-        setGameClass((prev: any) => {
-            const otherAttributes = prev.attributes.filter((eachAttribute: any) => eachAttribute.name !== attribute); 
-            return ({...prev, attributes: [otherAttributes, ]})
-        })
+        const newAttributes = [...gameClass.attributes];
+        newAttributes[i] = {name, value};
+        setGameClass({...gameClass, attributes: newAttributes});
     }
 
 
@@ -20,8 +19,8 @@ export default function classBar({gameClass, setGameClass}: {gameClass: any, set
         <div className="min-h-screen w-1/6 flex flex-col bg-slate-600 border-2 border-slate-400 p-4">
             <h1>{`Class name: ${gameClass.name}`}</h1>
             <ul className=" bg-slate-500 w-full ">
-                {gameClass.attributes.map((attribute: attribute) => (
-                    <li><span>{`${attribute.name}:`}<input name={attribute.name} value={attribute.value} onChange={(e) => updateAttributes(e, attribute)}></input></span></li>
+                {gameClass.attributes.map((attribute: attribute, index: number) => (
+                    <li><span>{`${attribute.name}:`}<input name={attribute.name} value={attribute.value} onChange={(e) => updateAttributes(e, index)}/></span></li>
                 ))}
             </ul>
         </div>
