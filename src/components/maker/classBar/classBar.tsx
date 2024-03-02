45f5b628abf46e31c060ca57dbdb0d1ca366a941
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-03-01 23:12:57 -0500

    did something probably i think

--- src/components/maker/classBar/classBar.tsx
+++ src/components/maker/classBar/classBar.tsx
@@ -3,7 +3,7 @@ interface attribute {
     value: string
 }
 
-export default function classBar({gameClass, setGameClass}: {gameClass: any, setGameClass: any}) {
+export default function ClassBar({gameClass, setGameClass}: {gameClass: any, setGameClass: any}) {
     
     const updateAttributes = (e: React.ChangeEvent<HTMLInputElement>, i: number) => {
         e.preventDefault()
@@ -16,11 +16,16 @@ export default function classBar({gameClass, setGameClass}: {gameClass: any, set
 
 
     return(
-        <div className="min-h-screen w-1/6 flex flex-col bg-slate-600 border-2 border-slate-400 p-4">
+        <div className="min-h-screen w-3/12 flex flex-col bg-slate-500 border-2 border-slate-400 p-4">
             <h1>{`Class name: ${gameClass.name}`}</h1>
-            <ul className=" bg-slate-500 w-full ">
+            <ul className=" bg-slate-600 w-full p-4">
                 {gameClass.attributes.map((attribute: attribute, index: number) => (
-                    <li><span>{`${attribute.name}:`}<input name={attribute.name} value={attribute.value} onChange={(e) => updateAttributes(e, index)}/></span></li>
+                    <li key={index}>
+                        <div className="grid grid-cols-2">
+                            <span>{`${attribute.name}:`}</span>
+                            <input name={attribute.name} value={attribute.value} onChange={(e) => updateAttributes(e, index)}/>
+                        </div>
+                    </li>   
                 ))}
             </ul>
         </div>
