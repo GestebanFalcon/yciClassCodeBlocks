4daba23aa54728b695a2668b3dfc7dce291e20f5
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-03-17 19:20:18 -0400

    fix error

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,14 +1,27 @@
 "use client"
 import ClassBar from "@/components/maker/classBar/classBar";
+import Display from "@/components/activityDisplay/display";
 import { useState } from "react";
+
+// Import Blockly core.
+import * as Blockly from 'blockly/core';
+// Import the default blocks.
+import * as libraryBlocks from 'blockly/blocks';
+// Import a generator.
+import {javascriptGenerator} from 'blockly/javascript';
+// Import a message file.
+import * as En from 'blockly/msg/en';
+
+
 
 export default function Home() {
   //test
-  const [jawn, setJawn] = useState({name: 'person', attributes: [{name: 'height', value: 'short'}, {name: 'hair', value: 'brown'}], methods: []});
+  // const [jawn, setJawn] = useState({name: 'person', attributes: [{name: 'height', value: 'short'}, {name: 'hair', value: 'brown'}], methods: []});
 
   return (
     <>
-      <ClassBar gameClass={jawn} setGameClass={setJawn}/>
+      {/* <ClassBar gameClass={jawn} setGameClass={setJawn}/> */}
+      <Display/>
     </>
   );
 }
