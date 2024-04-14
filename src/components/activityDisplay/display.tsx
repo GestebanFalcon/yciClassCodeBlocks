de9b988fa41a1b07500dc98eee0410b0b61db3c0
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-04-14 19:31:34 -0400

    blockly works

--- src/components/activityDisplay/display.tsx
+++ src/components/activityDisplay/display.tsx
@@ -1,13 +1,13 @@
 // 3 section view
 "use client"
 import { useEffect, useRef, useState } from 'react';
-import { ResizableBox, ResizableProps } from 'react-resizable';
+import { Workspace } from './googleBlockly/workspace';
+import Toolbox from './googleBlockly/toolbox';
 
 export default function Display() {
     const [toolWidth, setToolWidth] = useState(300);
     const toolBorderRef: any = useRef(null);
     const toolRef: any = useRef(null)
-    
     
 
     const onMouseDown = (event: MouseEvent) => {
@@ -22,25 +22,25 @@ export default function Display() {
     };
 
     useEffect(() => {
-        let xCord = 0;
-        xCord = toolBorderRef.current.style.left;
+        // let xCord = 0;
+        // xCord = toolBorderRef.current.style.left;
 
-        addEventListener('mousedown', onMouseDown);
+        // addEventListener('mousedown', onMouseDown);
     }, [])
 
     return(
         <div className=" flex h-full flex-row" id="activityDisplayContainer">
-            <div className='absolute h-full bg-green-500' style={{width: toolWidth}}>
-                <div id="toolboxContainer" className='absolute h-full w-full p-4'>
-                    {/* <Toolbox/> */}
-                    
-                </div>
-                  {/* border */}
-                <div ref={toolBorderRef} className='absolute z-20 w-2 bg-black h-full hover:cursor-ew-resize' style={{ left: toolWidth - 4}}></div>
-            </div>
+            {/* <div className='absolute h-full bg-green-500' style={{width: toolWidth}}>
 
-            <div className="p-4 absolute w-1/3 h-full bg-red-500" style={{ left: toolWidth}}>
-                <button>workspace</button>
+                <div className='absolute h-full w-full p-4'>
+                    <Toolbox/>
+                </div>
+
+                <div ref={toolBorderRef} className='absolute z-20 w-2 bg-black h-full hover:cursor-ew-resize' style={{ left: toolWidth - 4}}></div>
+            </div> */}
+
+            <div className="p-4 absolute w-2/3 h-full bg-red-500" style={{ left: 0}}>
+                <Workspace/>
             </div>
 
             {/* <div className="absolute w-1/3 h-full bg-blue-500">
--- src/components/activityDisplay/googleBlockly/toolbox.tsx
+++ src/components/activityDisplay/googleBlockly/toolbox.tsx
@@ -1,41 +1,45 @@
-// "use client"
-// // Import Blockly core.
-// import * as Blockly from 'blockly/core';
-// // Import the default blocks.
-// import * as libraryBlocks from 'blockly/blocks';
-// // Import a generator.
-// import {javascriptGenerator} from 'blockly/javascript';
-// // Import a message file.
-// import * as En from 'blockly/msg/en';
+"use client"
+// Import Blockly core.
+import * as Blockly from 'blockly/core';
+// Import the default blocks.
+import * as libraryBlocks from 'blockly/blocks';
+// Import a generator.
+import {javascriptGenerator} from 'blockly/javascript';
+// Import a message file.
+import * as En from 'blockly/msg/en';
+import { useEffect } from 'react';
 
 
 
-// export function Toolbox() {
+export default function Toolbox() {
 
-//     const toolbox = {
-//         // There are two kinds of toolboxes. The simpler one is a flyout toolbox.
-//         kind: 'flyoutToolbox',
-//         // The contents is the blocks and other items that exist in your toolbox.
-//         contents: [
-//           {
-//             kind: 'block',
-//             type: 'controls_if'
-//           },
-//           {
-//             kind: 'block',
-//             type: 'controls_whileUntil'
-//           }
-//           // You can add more blocks to this array.
-//         ]
-//       };
+    // const toolbox = {
+    //     // There are two kinds of toolboxes. The simpler one is a flyout toolbox.
+    //     kind: 'flyoutToolbox',
+    //     // The contents is the blocks and other items that exist in your toolbox.
+    //     contents: [
+    //       {
+    //         kind: 'block',
+    //         type: 'controls_if'
+    //       },
+    //       {
+    //         kind: 'block',
+    //         type: 'controls_whileUntil'
+    //       }
+    //       // You can add more blocks to this array.
+    //     ]
+    //   };
       
-//       // The toolbox gets passed to the configuration struct during injection.
-//       const workspace = Blockly.inject('toolboxContent', {toolbox: toolbox});
+    //   // The toolbox gets passed to the configuration struct during injection.
+    //   useEffect(() => {
+    //     const workspace = Blockly.inject('toolboxContent', {toolbox: toolbox});
+    //   }, []);
+      
       
 
-//     return (
-//     <div id="toolboxContent">
+    return (
+    <div id="toolboxContent">
 
-//     </div>
-//     )
-// }
+    </div>
+    )
+}
