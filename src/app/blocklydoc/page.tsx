317d135714ce3945d1e5933d7ee0826b80c627ea
Aaditya Shah <aadityashah02@gmail.com>
2024-04-14 18:46:44 -0500

    begin dev

--- src/app/blocklydoc/page.tsx
+++ src/app/blocklydoc/page.tsx
@@ -1,18 +1,16 @@
-"use client"
+"use client";
 import ClassBar from "@/components/maker/classBar/classBar";
 import Display from "@/components/activityDisplay/display";
 import { useState } from "react";
 
 // Import Blockly core.
-import * as Blockly from 'blockly/core';
+import * as Blockly from "blockly/core";
 // Import the default blocks.
-import * as libraryBlocks from 'blockly/blocks';
+import * as libraryBlocks from "blockly/blocks";
 // Import a generator.
-import {javascriptGenerator} from 'blockly/javascript';
+import { javascriptGenerator } from "blockly/javascript";
 // Import a message file.
-import * as En from 'blockly/msg/en';
-
-
+import * as En from "blockly/msg/en";
 
 export default function Home() {
   //test
@@ -21,7 +19,7 @@ export default function Home() {
   return (
     <>
       {/* <ClassBar gameClass={jawn} setGameClass={setJawn}/> */}
-      <Display/>
+      <Display />
     </>
   );
 }
