3fb716a8aab39c2ecddb9e95cb480633df91e3ac
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-10-27 22:11:05 -0400

    map update

--- src/app/(site)/info/map/page.tsx
+++ src/app/(site)/info/map/page.tsx
@@ -7,8 +7,10 @@ export default function Map() {
                 <p><b>10/27/2024</b> - Release of beta: basic movement and procedural programming</p><br></br>
                 <h2>November</h2>
                 <p><b>11/3/2024</b> - Integration to variables and data, selection, and boolean operations</p><br></br>
-                <p><b>11/10/2024</b> - Implement sprite animations to make the game more vivid</p>
-
+                <p><b>11/10/2024</b> - Implement sprite animations to make the game more vivid</p><br></br>
+                <p><b>11/24/2024</b> - New adventure introducing functional programming</p><br></br>
+                <h2>December</h2>
+                <p><b>12/1/2024</b> - Add lists and iteration to the new adventure from before</p><br></br>
 
             </div>
         </div>
