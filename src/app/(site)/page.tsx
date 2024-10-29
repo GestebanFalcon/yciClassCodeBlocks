dd2f140f94becfff6d0cf52de26772758c9b03c1
Gustavo Falcon <gustavoestebanfalcon@gmail.com>
2024-10-28 21:49:37 -0400

    fixed the annoying merge changes

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,26 +1,33 @@
-
-"use client";
+"use client"
 import ClassBar from "@/components/maker/classBar/classBar";
 import Display from "@/components/activityDisplay/display";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 // Import Blockly core.
-import * as Blockly from "blockly/core";
+import * as Blockly from 'blockly/core';
 // Import the default blocks.
-import * as libraryBlocks from "blockly/blocks";
+import * as libraryBlocks from 'blockly/blocks';
 // Import a generator.
-import { javascriptGenerator } from "blockly/javascript";
+import { javascriptGenerator } from 'blockly/javascript';
 // Import a message file.
-import * as En from "blockly/msg/en";
+import * as En from 'blockly/msg/en';
+import { useRouter } from "next/navigation";
+
+
 
 export default function Home() {
+    const router = useRouter();
     //test
     // const [jawn, setJawn] = useState({name: 'person', attributes: [{name: 'height', value: 'short'}, {name: 'hair', value: 'brown'}], methods: []});
-
+    useEffect(() => {
+        router.push("/info");
+    }, [])
     return (
         <>
             {/* <ClassBar gameClass={jawn} setGameClass={setJawn}/> */}
-            <Display />
+            <div id="main content">
+
+            </div>
         </>
     );
 }
